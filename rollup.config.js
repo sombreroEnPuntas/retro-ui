a1d13580a0d9eee9e897b4995c5a11d930553d3e
Mati <matiasfornes@gmail.com>
2020-01-03 21:45:50 +0100

    feat: bundle lib with rollup

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      exclude: ['*.test.ts+(|x)', '**/*.test.ts+(|x)'],
+      clean: true,
+    }),
+    commonjs({
+      include: ['node_modules/**'],
+      namedExports: {
+        'node_modules/react/index.js': [
+          'cloneElement',
+          'createContext',
+          'Component',
+          'createElement',
+        ],
+        'node_modules/react-dom/index.js': ['render'],
+        'node_modules/react-is/index.js': [
+          'isElement',
+          'isValidElementType',
+          'ForwardRef',
+        ],
+      },
+    }),
+  ],
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Box from './components/Box'
+import Button from './components/Button'
+
+import ThemeWrapper from './theme/ThemeWrapper'
+
+export { Box, Button, ThemeWrapper }
