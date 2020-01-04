6621593a53392197d1ad79a583b411fd30781b86
Mati <matiasfornes@gmail.com>
2020-01-04 20:02:42 +0100

    fix: drop rollup node resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
@@ -25,7 +24,6 @@ export default {
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
-    resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
       clean: true,
