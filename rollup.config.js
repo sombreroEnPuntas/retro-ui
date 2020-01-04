291f4d8516d17f8d0d0eb043981c3a3b6b7da1d3
Mati <matiasfornes@gmail.com>
2020-01-04 19:57:34 +0100

    fix: drop rollup external plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
 
@@ -21,8 +20,11 @@ export default {
       sourcemap: true,
     },
   ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
   plugins: [
-    external(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
