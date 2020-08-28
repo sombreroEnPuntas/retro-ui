53fa2244e65b8818f96c8bca89b058aee04ca27d
Mati <matiasfornes@gmail.com>
2020-08-28 13:07:06 +0200

    feat: message component

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import Box from './components/Box'
 import Button from './components/Button'
 import Input from './components/Input'
+import Message from './components/Message'
 import Modal from './components/Modal'
 
 import ThemeWrapper from './theme/ThemeWrapper'
 
-export { Box, Button, Input, Modal, ThemeWrapper }
+export { Box, Button, Input, Message, Modal, ThemeWrapper }
