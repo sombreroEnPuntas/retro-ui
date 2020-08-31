32a1584b9e0207b4b22091295dff183c5161891a
Mati <matiasfornes@gmail.com>
2020-08-31 12:14:43 +0200

    chore: format colors to lowercase

--- src/components/Box.test.tsx
+++ src/components/Box.test.tsx
@@ -17,7 +17,7 @@ describe.each`
   ${'Box'}         | ${{}}                  | ${'background-color: white; box-shadow: 0 4px #4d5256,0 -4px #4d5256,4px 0 #4d5256,-4px 0 #4d5256; color: #212529;'}
   ${'Box error'}   | ${{ type: 'error' }}   | ${'background-color: #ffd7cf; box-shadow: 0 4px #e76e55,0 -4px #e76e55,4px 0 #e76e55,-4px 0 #e76e55; color: #ce372b;'}
   ${'Box success'} | ${{ type: 'success' }} | ${'background-color: #c9d68f; box-shadow: 0 4px #92cc41,0 -4px #92cc41,4px 0 #92cc41,-4px 0 #92cc41; color: #76c442;'}
-  ${'Box warning'} | ${{ type: 'warning' }} | ${'background-color: #FFE5A6; box-shadow: 0 4px #E9C46A,0 -4px #E9C46A,4px 0 #E9C46A,-4px 0 #E9C46A; color: #B38106;'}
+  ${'Box warning'} | ${{ type: 'warning' }} | ${'background-color: #ffe5a6; box-shadow: 0 4px #e9c46a,0 -4px #e9c46a,4px 0 #e9c46a,-4px 0 #e9c46a; color: #b38106;'}
 `(`$displayName`, ({ props, css }) => {
   it(`renders with ${css}`, () => {
     const { getByText } = render(
