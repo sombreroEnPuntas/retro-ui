92635df629ad44f70cb5ad47c559a78de318e4a2
Mati <matiasfornes@gmail.com>
2020-08-31 12:36:18 +0200

    feat: responsive CTAs
    On touch-operated devices, meat-based life forms will try to hit
    only ONE CTA at the time using rather unreliable probes (fingers mostly).
    Make them bigger to aid them in their feeble goals.
    
    ![ET-touch friendly](https://media.giphy.com/media/nL4HKlfoUBW7kO0vqu/giphy.gif)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,22 @@
 import { styled } from '../theme/theme'
 
 export const Button = styled.button`
-  ${({ theme: { colors, fonts, snippets } }) => `
+  ${({ theme: { breakpoints, colors, fonts, snippets } }) => `
     background-color: ${colors.black.background};
     color: ${colors.black.outline};
     cursor: pointer;
     display: block;
     font-family: ${fonts.fontFamily};
-    font-size: 0.65em;
+    font-size: ${fonts.lineHeight};
     font-weight: bold;
     line-height: calc(2 * ${fonts.lineHeight});
     padding: 0.5em;
 
     ${snippets.boxShadow({ colors, type: 'black' })}
+
+    ${breakpoints['desktop']} {
+      line-height: ${fonts.lineHeight};
+    }
   `}
 `
 
